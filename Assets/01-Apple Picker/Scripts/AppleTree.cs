5124db3dc194391456ae51563b51878f524a8598
osmith0705 <osmith0705@gmail.com>
2024-02-01 20:35:38 -0500

    Made all Game Object/Prefabs + Started AppleTree Script. pg471

---
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    //Apple prefab
+    public GameObject applePrefab;
+
+    //speed of movement
+    public float speed = 1f;
+
+    //boundary
+    public float edge = 10f;
+
+    //chance to turn
+    public float chanceToChangeDirections = .1f;
+
+    //drop apple frequency
+    public float seconds = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
