a0e4018058de4e392235f8818d2231096fbf4639
osmith0705 <osmith0705@gmail.com>
2024-02-04 17:30:46 -0500

    pg. 480

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -28,6 +28,32 @@ public class AppleTree : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if(pos.x < -edge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if(pos.x > edge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
         
+    }
+    private void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+    }
+    
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", seconds);
     }
 }
