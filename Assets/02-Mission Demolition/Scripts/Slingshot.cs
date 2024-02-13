38debb6984194109bbdf082cfbb32ddc03574edb
osmith0705 <osmith0705@gmail.com>
2024-02-12 20:23:00 -0500

    Final

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -13,6 +13,7 @@ public class Slingshot : MonoBehaviour
     public Vector3 launchPos;
     public GameObject projectile;
     public bool aimingMode;
+    private Rigidbody projRigidbody;
 
     void Awake()
     {
@@ -30,7 +31,33 @@ public class Slingshot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!aimingMode) { return; }
+
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 mouseDelta = mousePos3D - launchPos;
+
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            aimingMode = false;
+            projRigidbody.isKinematic = false;
+            projRigidbody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
+            projectile = null;
+        }
     }
 
     void OnMouseEnter()
@@ -54,6 +81,9 @@ public class Slingshot : MonoBehaviour
         projectile.transform.position = launchPos;
 
         projectile.GetComponent<Rigidbody>().isKinematic = true;
+
+        projRigidbody = projectile.GetComponent <Rigidbody>();
+        projRigidbody.isKinematic = true;
     }
 
 
